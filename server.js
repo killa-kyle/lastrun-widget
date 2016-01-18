23065602cf010c2175f6ce66f459c81ed57ad774
killa-kyle <kylerose@gmail.com>
2016-01-18 00:03:33 -0500

    variable port change for heroku

--- server.js
+++ server.js
@@ -42,9 +42,4 @@ strava.athlete.listActivities({type: 'run'},function(err,payload) {
 
 
 
-var server = app.listen(5000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-});
+var server = app.listen(process.env.PORT || 5000);
